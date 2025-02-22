e5d1ca6cbad0f0659bc671692bf3413cefdf8c04
travisfriesen <github@travisfriesen.ca>
2025-02-22 11:04:16 -0600

    a fleeing attempt to get this working

--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -1,29 +1,52 @@
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+
 export interface ITextComponent {
-    bold?: string
-    normal?: string
-    isBullet?: boolean
+    bold?: string;
+    normal?: string;
+    isBullet?: boolean;
+    onBoldChange: (bold: string) => void;
+    onNormalChange: (normal: string) => void;
 }
 
-export default function TextComponent({ bold, normal, isBullet }: ITextComponent) {
-    return(
-        <div className={`flex flex-row p-1 gap-2`}>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                value={bold}
-                placeholder={isBullet ? "Bullet Bold Text" : "Bold Text"}
-                className="block min-w-0 py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-            />
+export default function TextComponent({ bold, normal, isBullet, onBoldChange, onNormalChange }: ITextComponent) {
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            bold: bold,
+            normal: normal
 
-            <input
-                id="name"
-                name="name"
-                type="text"
-                value={normal}
-                placeholder={isBullet ? "Bullet Normal Text" : "Normal Text"}
-                className="block min-w-0 py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+        }
+    });
+
+    const onSubmit = (data: { bold?: string; normal?: string }) => {
+        onBoldChange(data.bold || '');
+        onNormalChange(data.normal || '');
+    };
+
+    return (
+        <form onBlur={handleSubmit(onSubmit)} className={`flex flex-row p-1 gap-2`}>
+            <Controller
+                name="bold"
+                control={control}
+                render={({ field }) => (
+                    <input
+                        {...field}
+                        placeholder="Bold Text  "
+                        className="block min-w-0 py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                    />
+                )}
             />
-        </div>
-    )
+            <Controller
+                name="normal"
+                control={control}
+                render={({ field }) => (
+                    <input
+                        {...field}
+                        placeholder="Normal Text"
+                        className="block min-w-0 py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                    />
+                )}
+            />
+        </form>
+    );
 }
