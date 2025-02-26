f70017400772813f3660b1405a324199f01ff274
travisfriesen <github@travisfriesen.ca>
2025-02-25 20:13:04 -0600

    chore: cont. refactored into better folder structure

--- CommitCV/src/app/layout.tsx
+++ CommitCV/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { FileProvider } from "@/context/FileContext";
+import { FileProvider } from "@/data/context/FileContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
