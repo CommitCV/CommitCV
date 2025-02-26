d7c42eb62a65bd7fe4ccb407798cfe427067bb81
travisfriesen <github@travisfriesen.ca>
2025-02-25 19:42:07 -0600

    chore: fixed console error in dev environment

--- CommitCV/next.config.ts
+++ CommitCV/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    webpack: (config) => {
+    webpack: (config, {dev}) => {
       config.resolve.alias.canvas = false;
-
+        if (dev) {
+            config.devtool = 'source-map';
+        }
       return config;
     },
     experimental: {
