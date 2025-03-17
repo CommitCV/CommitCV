8b516a51bc350481b09b84dea52c1068efcc7779
Travis <github@travisfriesen.ca>
2025-03-16 21:37:40 -0500

    feat: fix bullet and add github link to header (#36)
    * fix: bullet collection bold element cloning text
    
    * feat: added github link to header
    
    * chore: update photo for readme

--- CommitCV/src/components/Header.tsx
+++ CommitCV/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Red_Hat_Mono } from "next/font/google";
 import Link from "next/link";
+import { IoLogoGithub } from "react-icons/io";
 
 const rhm = Red_Hat_Mono({ subsets: ['latin'] })
 
@@ -7,8 +8,14 @@ export default function Header() {
     return (
         <div>
             <Link href={`/`}>
-                <div className={`flex flex-row pt-2 pl-2`}>
+                <div className={`flex flex-row p-2`}>
                     <h1 className={`${rhm.className} font-thin font text-6xl`}>Commit</h1> <h1 className={`${rhm.className} font-extrabold text-6xl`}>CV</h1>
+                    <div className={`ml-auto flex flex-row items-center`}>
+                        <h2 className={`mr-2 ${rhm.className}`}>Check out our code on</h2>
+                        <Link href={"https://github.com/CommitCV/CommitCV"}>
+                            <IoLogoGithub size={42}/>
+                        </Link>
+                    </div>
                 </div>
             </Link>
         </div>
