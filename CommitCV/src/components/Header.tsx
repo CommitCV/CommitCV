07716dd09fcab3f4f53d54038631a864971134cc
Travis <github@travisfriesen.ca>
2025-03-18 20:19:23 -0500

    feat: made pdfCV async, chore: bump version number, fix: header link issue (#37)
    * feat: made async file process, and updated cors to accept multiple urls
    
    * chore: bump version number
    
    * chore: updated docker file to support default port
    
    * chore: update .env.example file
    
    * chore: bump commitcv version
    
    * fix: removed overlap buttons

--- CommitCV/src/components/Header.tsx
+++ CommitCV/src/components/Header.tsx
@@ -7,17 +7,17 @@ const rhm = Red_Hat_Mono({ subsets: ['latin'] })
 export default function Header() {
     return (
         <div>
-            <Link href={`/`}>
-                <div className={`flex flex-row p-2`}>
+            <div className={`flex flex-row p-2`}>
+                <Link href={`/`}>
                     <h1 className={`${rhm.className} font-thin font text-6xl`}>Commit</h1> <h1 className={`${rhm.className} font-extrabold text-6xl`}>CV</h1>
-                    <div className={`ml-auto flex flex-row items-center`}>
-                        <h2 className={`mr-2 ${rhm.className}`}>Check out our code on</h2>
-                        <Link href={"https://github.com/CommitCV/CommitCV"}>
-                            <IoLogoGithub size={42}/>
-                        </Link>
-                    </div>
+                </Link>
+                <div className={`ml-auto flex flex-row items-center`}>
+                    <h2 className={`mr-2 ${rhm.className}`}>Check out our code on</h2>
+                    <Link href={"https://github.com/CommitCV/CommitCV"}>
+                        <IoLogoGithub size={42}/>
+                    </Link>
                 </div>
-            </Link>
+            </div>
         </div>
     )
 }
