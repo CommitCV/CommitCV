ec9b6adc4842629d56c66baf79830b022868cfe0
cjlangan <connorjameslangan@gmail.com>
2026-01-08 22:49:44 -0600

    feat: altered example components to use theming and made test theme switcher

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -6,12 +6,12 @@ export default function About() {
     return (
         <>
             <Header />
-            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-800 gap-8">
+            <div className="min-h-screen flex flex-col items-center justify-center bg-bg-1 text-fg-1 gap-8">
                 <h1 className="text-4xl font-bold tracking-tight">
                     The Example About Page
                 </h1>
 
-                <p className="text-sm text-gray-500">
+                <p className="text-sm text-fg-3">
                     Here there will be information regarding CommitCV
                 </p>
 
